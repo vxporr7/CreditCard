f5f6e31dbd96a26b2c36027b23b17b4119aa5e11
Kunmi Aaron <aaronob@mail.uc.edu>
2024-09-20 14:00:30 -0400

    Credit card balance

---
+++ src/CcBalance.java
@@ -0,0 +1,13 @@
+public class CcBalance {
+    public static void main(String[] args) {
+        System.out.println("You have $5000 as credit before interest!");
+
+        double preInterest = 5000;
+        double interestRate = 0.17;
+        double interestCost1 = preInterest * interestRate;
+        double interestCost2 = interestCost1 * interestRate;
+
+        System.out.println("Your credit card balance after month 1 is " + interestCost1);
+        System.out.println("Your credit card balance after month 2 is " + interestCost2);
+    }
+}
