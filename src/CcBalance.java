b98affd52d8b76f06ee8d8fb75cb7f2331df4e0d
Kunmi Aaron <aaronob@mail.uc.edu>
2024-09-20 14:02:00 -0400

    Credit card balance, added comment slash

--- src/CcBalance.java
+++ src/CcBalance.java
@@ -6,6 +6,7 @@ public class CcBalance {
         double interestRate = 0.17;
         double interestCost1 = preInterest * interestRate;
         double interestCost2 = interestCost1 * interestRate;
+        //
 
         System.out.println("Your credit card balance after month 1 is " + interestCost1);
         System.out.println("Your credit card balance after month 2 is " + interestCost2);
